cad13fc33c55207f69093f8e246d249244f1c4f2
hernanguaragna <hernanguaragna@gmail.com>
2022-11-28 16:57:38 -0300

    terminada-push final

--- 15-cocktails/setup/src/pages/About.js
+++ 15-cocktails/setup/src/pages/About.js
@@ -1,14 +1,16 @@
-import React from 'react'
+import React from "react";
 
-const About = () => {
+export default function About() {
   return (
-		<section className="section about-section">
-			<h1 className="section-title">about us</h1>
-			<p>Lorem ipsum dolor sit amet consectetur adipiscing elit vulputate etiam, sem netus praesent cubilia dictum velit felis ultricies hendrerit, molestie eget montes fermentum et varius commodo non. Inceptos imperdiet lobortis senectus viverra vitae tempus augue hac, aliquet justo class torquent vulputate sociosqu taciti mus, curabitur lacinia turpis volutpat scelerisque ultricies lectus. Litora ornare metus neque congue donec leo odio parturient quisque, potenti sed suscipit aptent molestie ut aenean nisi etiam, convallis lobortis phasellus proin ac nibh feugiat consequat.
-
-</p>
-		</section>
-	);
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae
+        repudiandae architecto qui adipisci in officiis, aperiam sequi atque
+        perferendis eos, autem maiores nisi saepe quisquam hic odio consectetur
+        nobis veritatis quasi explicabo obcaecati doloremque? Placeat ratione
+        hic aspernatur error blanditiis?
+      </p>
+    </section>
+  );
 }
-
-export default About
